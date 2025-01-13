632f91a1f369337e782da22d8665b7eda3a2ad36
Sagar S <contact.sagar.srivastava@gmail.com>
2025-01-13 12:43:02 +0530

    initia commit b1

--- src/basic/Test2.java
+++ src/basic/Test2.java
@@ -2,7 +2,7 @@ package basic;
 
 public class Test2 {
 	public static void main(String[] args) {
-		System.out.println("File 2 change in main");
+		System.out.println("File 2 change in b1-This change is done in branch b1");
 	}
 
 }
